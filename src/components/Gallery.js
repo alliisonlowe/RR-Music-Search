7af8cb1a2e53dbd2a7e74b04ad1bb28a5a8317fb
Allison Lowe <alliisonlowe@gmail.com>
2022-12-20 16:35:38 -0800

    updating main

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,10 +1,15 @@
-// Gallery.js
 import GalleryItem from './GalleryItem'
 
 function Gallery(props){
+
+    const display = props.data.map((item,index) => {
+        return (
+            <GalleryItem item={item} key={index} />
+        )
+    })
     return (
         <div>
-            <GalleryItem />
+            {display}
         </div>
     )
 }
